5b62265e9fd0e978dd0eaa26f661f0d29d5a65b4
Ed Crookshanks <ed.crookshanks@gmail.com>
2022-10-08 08:36:08 -0400

    initial commit

---
+++ src/main/java/com/nokelservices/testsimple/TestSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.nokelservices.testsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nokelservices/testsimple/TestSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokelservices.testsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
